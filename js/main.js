d3103c052add91e57a48f462969d8ae5208737cd
Владимир Ливинский <xDamneDx@Gmail.com>
2019-06-13 18:49:32 +0300

    Добавляет исправления учитывая рекомендации наставника.

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ var getOffersData = function (quantity) {
         avatar: getAvatarSrc(avatarNumber + 1)
       },
       offer: {
-        type: offers[getRandomNumber(0, offers.length + 1)]
+        type: offers[getRandomNumber(0, offers.length - 1)]
       },
       location: {
         x: getRandomNumber(0, mapWidth),
@@ -50,7 +50,8 @@ var offers = getOffersData(QUANTITY_OF_OFFERS);
 
 var renderPin = function (offer) {
   var pinElement = pinTemplate.cloneNode(true);
-  pinElement.style = 'left: ' + (offer.location.x - (PIN.width / 2)) + 'px; top: ' + (offer.location.y - PIN.height) + 'px;';
+  pinElement.style.left = (offer.location.x - (PIN.width / 2)) + 'px';
+  pinElement.style.top = (offer.location.y - PIN.height) + 'px';
   pinElement.querySelector('img').src = offer.author.avatar;
   pinElement.querySelector('img').alt = offer.offer.type;
   return pinElement;
